9c1da5c6ce5e56df055210019b799c1e9d8fd5ca
Mama <KsuNami@yandex.ru>
2023-05-01 12:30:47 +0500

    two

---
+++ mockup/src/src/component/Pages/Home.js
@@ -0,0 +1,16 @@
+import '../../App.css';
+
+import Card from '../Card/Card';
+import Footer from '../Footer/Footer';
+import Slider from '../Slider/Slider';
+
+const Home = () => {
+    return(
+        <>
+        <Card/>
+        <Slider/>
+        <Footer/>
+        </>
+    )
+}
+export default Home;
